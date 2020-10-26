5aa8876db1995ecdbc44074a603654e426f6251c
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-26 00:40:22 -0400

    might still do a toString method

--- project1/src/dev/tylerdclark/two_dimensional/Circle.java
+++ project1/src/dev/tylerdclark/two_dimensional/Circle.java
@@ -1,2 +1,31 @@
-package dev.tylerdclark.two_dimensional;public class Circle {
+/*
+ * *****************************************************************************
+ * NAME: Tyler D Clark
+ * PROJECT: Project 1
+ * COURSE: CMSC 335
+ * DATE: 25 OCT 2020
+ * *****************************************************************************
+ */
+package dev.tylerdclark.two_dimensional;
+
+public class Circle extends TwoDimensionalShape {
+
+    private final int radius;
+
+    /**
+     * Constructor method to initialize radius field and calculate {@link #area()}
+     * @param radius length of the line from the center to any point on circle's edge
+     */
+    public Circle (int radius){
+        this.radius = radius;
+    }
+
+    /**
+     * Uses formula: pi * r^2 to calculate the area of this circle
+     * @return the area of a circle based on given radius
+     */
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(radius, 2);
+    }
 }
