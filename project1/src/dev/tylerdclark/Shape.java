c40ef71bb3bf4b596fa940fc2e1f71ddc296674d
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-26 00:40:58 -0400

    too much documentation

--- project1/src/dev/tylerdclark/Shape.java
+++ project1/src/dev/tylerdclark/Shape.java
@@ -1,2 +1,19 @@
-package dev.tylerdclark;public class shape {
+/*
+ * *****************************************************************************
+ * NAME: Tyler D Clark
+ * PROJECT: Project 1
+ * COURSE: CMSC 335
+ * DATE: 25 OCT 2020
+ * *****************************************************************************
+ */
+
+package dev.tylerdclark;
+
+/**
+ * This class exists mainly for other classes of project to extend. Made abstract
+ * since it is designed to be subclassed. It implicitly extends {@link Object}.
+ *
+ * @author tylerclark
+ */
+public abstract class Shape {
 }
