6a03ab196d318e5a624e158349cb06845685f026
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-26 00:39:58 -0400

    two abstracted class

---
+++ project1/src/dev/tylerdclark/three_dimensional/ThreeDimensionalShape.java
@@ -0,0 +1,26 @@
+/*
+ * *****************************************************************************
+ * NAME: Tyler D Clark
+ * PROJECT: Project 1
+ * COURSE: CMSC 335
+ * DATE: 25 OCT 2020
+ * *****************************************************************************
+ */
+
+package dev.tylerdclark.three_dimensional;
+
+import dev.tylerdclark.Shape;
+
+/**
+ * Class to be extended by Sphere, Cube, Cone, Cylinder, and Torus. Made abstract as no
+ * instantiations of this class will be needed and implementation of {@link #volume()} will
+ * depend on child class.
+ */
+
+public abstract class ThreeDimensionalShape extends Shape {
+   /**
+    * To be implemented with the volume formula specific to each shape.
+    * @return the result from the calculation of volume
+    */
+   public abstract float volume();
+}
---
+++ project1/src/dev/tylerdclark/two_dimensional/TwoDimensionalShape.java
@@ -0,0 +1,25 @@
+/*
+ * *****************************************************************************
+ * NAME: Tyler D Clark
+ * PROJECT: Project 1
+ * COURSE: CMSC 335
+ * DATE: 25 OCT 2020
+ * *****************************************************************************
+ */
+
+package dev.tylerdclark.two_dimensional;
+
+import dev.tylerdclark.Shape;
+
+/**
+ * Class to be extended by circle, square, triangle, and rectangle. Made abstract as no
+ * instantiations of this class will be needed and implementation of {@link #area()} will
+ * depend on child class.
+ */
+public abstract class TwoDimensionalShape extends Shape {
+    /**
+     * To be implemented with the area formula specific to each shape.
+     * @return the result from the calculation of area
+     */
+    public abstract double area();
+}
