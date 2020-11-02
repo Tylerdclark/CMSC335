7c49d4efd5c1935b153b836df9ff38eeb0b34a42
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-02 00:30:18 -0500

    Documentation

--- project1/src/dev/tylerdclark/Main.java
+++ project1/src/dev/tylerdclark/Main.java
@@ -1,8 +1,17 @@
+/*
+ * *****************************************************************************
+ * NAME: Tyler D Clark
+ * PROJECT: Project 1
+ * COURSE: CMSC 335
+ * DATE: 25 OCT 2020
+ * *****************************************************************************
+ */
+
 package dev.tylerdclark;
 
 public class Main {
 
     public static void main(String[] args) {
-	    new Menu();
+        new Menu();
     }
 }
