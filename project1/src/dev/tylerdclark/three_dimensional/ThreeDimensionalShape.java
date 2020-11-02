bb39f7f140837863ded20e3569a75c1ec26693a9
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-02 00:31:40 -0500

    documentation and including toString methods

--- project1/src/dev/tylerdclark/three_dimensional/ThreeDimensionalShape.java
+++ project1/src/dev/tylerdclark/three_dimensional/ThreeDimensionalShape.java
@@ -12,15 +12,22 @@ package dev.tylerdclark.three_dimensional;
 import dev.tylerdclark.Shape;
 
 /**
- * Class to be extended by Sphere, Cube, Cone, Cylinder, and Torus. Made abstract as no
- * instantiations of this class will be needed and implementation of {@link #volume()} will
- * depend on child class.
+ * Class to be extended by Sphere, Cube, Cone, Cylinder, and Torus. Made
+ * abstract as no instantiations of this class will be needed and implementation
+ * of {@link #volume()} will depend on child class. Calling toString will print
+ * volume information.
  */
 
 public abstract class ThreeDimensionalShape extends Shape {
    /**
     * To be implemented with the volume formula specific to each shape.
+    * 
     * @return the result from the calculation of volume
     */
    public abstract double volume();
+
+   @Override
+   public String toString() {
+      return String.format("The volume of the %s is %.1f.", getClass().getSimpleName(), volume());
+   }
 }
