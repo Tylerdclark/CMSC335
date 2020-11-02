60a4cce53122520df79257f80d8d6561887a5f8d
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-02 00:30:32 -0500

    Not needed

--- project1/src/dev/tylerdclark/Shape.java
+++ project1/src/dev/tylerdclark/Shape.java
@@ -12,8 +12,6 @@ package dev.tylerdclark;
 /**
  * This class exists mainly for other classes of project to extend. Made abstract
  * since it is designed to be subclassed. It implicitly extends {@link Object}.
- *
- * @author tylerclark
  */
 public abstract class Shape {
 }
