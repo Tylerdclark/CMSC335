9607a72b5b1e87709e622f0ec0138afe0b1f9239
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-07 00:21:20 -0500

    Change from the intellij craziness

--- project2/src/dev/tylerdclark/MenuGUI.java
+++ project2/src/dev/tylerdclark/MenuGUI.java
@@ -1,16 +1,26 @@
 package dev.tylerdclark;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuGUI extends JFrame {
-
-    private JPanel mainPanel;
+    JComboBox<Shape> shapeJComboBox;
 
     public MenuGUI(String title) {
         super(title);
 
+        JPanel introPanel = new JPanel();
+        introPanel.add(new JLabel("Please select a shape from the dropdown:"));
+
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setContentPane(mainPanel);
+        this.setContentPane(introPanel);
         this.pack();
+    }
+
+    private static void addComponents(Panel panel, Component[] components){
+        for (Component component: components) {
+            panel.add(component);
+        }
     }
 }
