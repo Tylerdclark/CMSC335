19acb68d2a848c4d89cd14792246ef61b4f978d6
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-12 22:35:12 -0500

    this works

--- project2/src/dev/tylerdclark/gui/SpecificationPanel.java
+++ project2/src/dev/tylerdclark/gui/SpecificationPanel.java
@@ -1,4 +1,20 @@
 package dev.tylerdclark.gui;
 
-public class SpecificationPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class SpecificationPanel extends JPanel {
+
+    public SpecificationPanel(String[] itemSpecifications) {
+        this.setLayout(new GridLayout(1, itemSpecifications.length));
+
+        for (String spec : itemSpecifications) {
+            JLabel itemSpecLabel = new JLabel(spec);
+            JTextField itemSpecTextField = new JTextField();
+
+            this.add(itemSpecLabel);
+            this.add(itemSpecTextField);
+
+        }
+    }
 }
