4df78bf79dce2799c5010241c94309720f678920
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-12 22:34:49 -0500

    Sort of just placeholder for now

--- project2/src/dev/tylerdclark/gui/DrawPane.java
+++ project2/src/dev/tylerdclark/gui/DrawPane.java
@@ -1,2 +1,14 @@
-package dev.tylerdclark.gui;public class DrawPane {
+package dev.tylerdclark.gui;
+
+import dev.tylerdclark.Shape;
+
+import javax.swing.*;
+
+public class DrawPane extends JFrame {
+    public DrawPane(Shape currentShape) {
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setSize(400, 400);
+
+        this.add(new JLabel("Heeeeelllllllllloooo!"/*currentShape.toString()*/));
+    }
 }
