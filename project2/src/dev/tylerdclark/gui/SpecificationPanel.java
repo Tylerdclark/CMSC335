d7a730450a37562fa89a4d5964e8595d44555831
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-14 21:40:09 -0500

    This looks passable for now

--- project2/src/dev/tylerdclark/gui/SpecificationPanel.java
+++ project2/src/dev/tylerdclark/gui/SpecificationPanel.java
@@ -2,19 +2,30 @@ package dev.tylerdclark.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class SpecificationPanel extends JPanel {
 
+    private final ArrayList<JTextField> textFields = new ArrayList<>();
+
     public SpecificationPanel(String[] itemSpecifications) {
+
         this.setLayout(new GridLayout(1, itemSpecifications.length));
 
         for (String spec : itemSpecifications) {
             JLabel itemSpecLabel = new JLabel(spec);
             JTextField itemSpecTextField = new JTextField();
 
+            textFields.add(itemSpecTextField);
+
             this.add(itemSpecLabel);
             this.add(itemSpecTextField);
 
+
         }
+    }
+
+    public ArrayList<JTextField> getTextFields() {
+        return textFields;
     }
 }
