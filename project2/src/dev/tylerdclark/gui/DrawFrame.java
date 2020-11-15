8c8d1c8aec9528bd4ab8c9949d0bd305adc8fc5e
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-14 21:41:22 -0500

    need to figure out how to center the drawing panel properly

--- project2/src/dev/tylerdclark/gui/DrawFrame.java
+++ project2/src/dev/tylerdclark/gui/DrawFrame.java
@@ -3,11 +3,26 @@ package dev.tylerdclark.gui;
 import dev.tylerdclark.Shape;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class DrawFrame extends JFrame {
+
+    private final Shape currentShape;
+
     public DrawFrame(Shape currentShape) {
+        getContentPane().setLayout(
+                //new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS)
+                new GridBagLayout()
+        );
+        //contentPanel.setBackground(Color.CYAN);
+        this.currentShape = currentShape;
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setSize(400, 400);
-        this.add(new JLabel(currentShape.toString()));
+        this.setSize(325, 350);
+
+
+    }
+    public void drawShape() throws IOException {
+        this.add(currentShape.getShape());
     }
 }
