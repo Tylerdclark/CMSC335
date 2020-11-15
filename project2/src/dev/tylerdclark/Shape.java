8a6fbcaa576b2ee4811db851951e6316ed76eab3
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-14 21:39:29 -0500

    add in the method stubs to be inherited

--- project2/src/dev/tylerdclark/Shape.java
+++ project2/src/dev/tylerdclark/Shape.java
@@ -10,6 +10,10 @@
 
 package dev.tylerdclark;
 
+import javax.swing.*;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * This class exists mainly for other classes of project to extend. Made abstract
  * since it is designed to be subclassed. It implicitly extends {@link Object}.
@@ -30,4 +34,16 @@ public abstract class Shape {
     public String toString() {
         return getClass().getSimpleName();
     }
+
+    /**
+     * Returns a JPanel containing a drawn shape based on the data passed to the object.
+     * @return returns a JPanel containing the shape
+     */
+    public abstract JPanel getShape() throws IOException;
+
+    /**
+     * Passes values to the objects in a way that shapes with varying parameter counts can implement.
+     * @param values an arraylist of integers. First index is the first initialized data member and so on.
+     */
+    public abstract void passValues(ArrayList<Integer> values);
 }
