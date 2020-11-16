425656c8c33e80aa26110cfdbe8f9bb40525b7a0
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-15 23:29:53 -0500

    comments

--- project2/src/dev/tylerdclark/Shape.java
+++ project2/src/dev/tylerdclark/Shape.java
@@ -15,19 +15,22 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * This class exists mainly for other classes of project to extend. Made abstract
- * since it is designed to be subclassed. It implicitly extends {@link Object}.
+ * This class exists mainly for other classes of project to extend. Made
+ * abstract since it is designed to be subclassed. It implicitly extends
+ * {@link Object}.
  */
 public abstract class Shape {
 
     /**
      * Used to properly display the required fields of each shape object
+     * 
      * @return an array of required fields for shape creation
      */
     public abstract String[] getSpecifications();
 
     /**
      * Prints the name of the class
+     * 
      * @return String name of the class
      */
     @Override
@@ -36,14 +39,19 @@ public abstract class Shape {
     }
 
     /**
-     * Returns a JPanel containing a drawn shape based on the data passed to the object.
+     * Returns a JPanel containing a drawn shape based on the data passed to the
+     * object.
+     * 
      * @return returns a JPanel containing the shape
      */
     public abstract JPanel getShape() throws IOException;
 
     /**
-     * Passes values to the objects in a way that shapes with varying parameter counts can implement.
-     * @param values an arraylist of integers. First index is the first initialized data member and so on.
+     * Passes values to the objects in a way that shapes with varying parameter
+     * counts can implement.
+     * 
+     * @param values an arraylist of integers. First index is the first initialized
+     *               data member and so on.
      */
     public abstract void passValues(ArrayList<Integer> values);
 }
