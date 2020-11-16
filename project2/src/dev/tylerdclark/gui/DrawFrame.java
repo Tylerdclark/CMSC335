586893bc97f3c834892032cec34895bbc8f4b52b
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-15 23:29:03 -0500

    This works for now, will comment soon

--- project2/src/dev/tylerdclark/gui/DrawFrame.java
+++ project2/src/dev/tylerdclark/gui/DrawFrame.java
@@ -3,7 +3,6 @@ package dev.tylerdclark.gui;
 import dev.tylerdclark.Shape;
 
 import javax.swing.*;
-import java.awt.*;
 import java.io.IOException;
 
 public class DrawFrame extends JFrame {
@@ -11,18 +10,15 @@ public class DrawFrame extends JFrame {
     private final Shape currentShape;
 
     public DrawFrame(Shape currentShape) {
-        getContentPane().setLayout(
-                //new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS)
-                new GridBagLayout()
-        );
-        //contentPanel.setBackground(Color.CYAN);
         this.currentShape = currentShape;
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setSize(325, 350);
-
+        this.setSize(350, 375);
 
     }
+
     public void drawShape() throws IOException {
-        this.add(currentShape.getShape());
+        JPanel shapePanel = currentShape.getShape();
+        this.setContentPane(shapePanel);
     }
+
 }
