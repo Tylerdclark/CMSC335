07fcdfb47b5770316e444cf43a4f4136a26e504a
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-15 23:29:32 -0500

    dont need the gridlayout. will comment tommorrow

--- project2/src/dev/tylerdclark/gui/SpecificationPanel.java
+++ project2/src/dev/tylerdclark/gui/SpecificationPanel.java
@@ -1,7 +1,6 @@
 package dev.tylerdclark.gui;
 
 import javax.swing.*;
-import java.awt.*;
 import java.util.ArrayList;
 
 public class SpecificationPanel extends JPanel {
@@ -10,17 +9,14 @@ public class SpecificationPanel extends JPanel {
 
     public SpecificationPanel(String[] itemSpecifications) {
 
-        this.setLayout(new GridLayout(1, itemSpecifications.length));
-
         for (String spec : itemSpecifications) {
             JLabel itemSpecLabel = new JLabel(spec);
-            JTextField itemSpecTextField = new JTextField();
+            JTextField itemSpecTextField = new JTextField(2);
 
             textFields.add(itemSpecTextField);
 
             this.add(itemSpecLabel);
             this.add(itemSpecTextField);
-
 
         }
     }
