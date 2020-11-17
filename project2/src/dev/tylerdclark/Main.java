cd3d959c71c96d6d7dda5329ebdd38a6a4ca9b55
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-16 20:50:51 -0500

    just comments

--- project2/src/dev/tylerdclark/Main.java
+++ project2/src/dev/tylerdclark/Main.java
@@ -1,7 +1,19 @@
+/*
+ * *****************************************************************************
+ * FILE: Main.java
+ * NAME: Tyler D Clark
+ * PROJECT: Project 2
+ * COURSE: CMSC 335
+ * DATE: 12 Nov 2020
+ * *****************************************************************************
+ */
 package dev.tylerdclark;
 
 import dev.tylerdclark.gui.MenuGUI;
 
+/**
+ * Main entry point for program. Creates the Main GUI object.
+ */
 public class Main {
 
     public static void main(String[] args) {
