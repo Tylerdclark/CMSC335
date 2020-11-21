9ed91ca2bfcbcbdbbcea83956845c1c536f0585f
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-20 21:10:53 -0500

    Hello world

--- project3/src/MainFrame.java
+++ project3/src/MainFrame.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(){
+        super("The title");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(400,400));
+        panel.add(new JLabel("hello!"));
+        setContentPane(panel);
+        pack();
+        setVisible(true);
+    }
 }
