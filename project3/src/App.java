0681a6eadc608995909fd9c72bc3c9dfd3bfa8c7
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-20 21:10:29 -0500

    First couple of files

---
+++ project3/src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    JFrame frame;
+
+    {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
