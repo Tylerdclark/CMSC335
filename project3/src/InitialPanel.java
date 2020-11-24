bdd107393b8b77a724c24c06c80123c331691b89
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-23 22:32:11 -0500

    Using gridbag because I'm too impatient for GUI tools

--- project3/src/InitialPanel.java
+++ project3/src/InitialPanel.java
@@ -1,18 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class InitialFrame extends JFrame {
+public class InitialPanel {
 
+    private final JPanel pnlMain;
 
+    public InitialPanel(){
 
-    public InitialFrame(){
-        /* All of the usual JFrame stuff */
-        super("Choose Initial conditions");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500,400);
-        setLayout(new GridLayout(7,2));
+        pnlMain = new JPanel();
+        pnlMain.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
         JLabel info = new JLabel("Please select options for the traffic simulation");
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(0, 10, 0, 10);
+        gbc.weightx = 1;
+        pnlMain.add(info, gbc);
+
+
+
+
+
         JLabel rowLabel = new JLabel("Row count:");
         JSlider rowSlider = new JSlider(JSlider.HORIZONTAL, 15);
         JLabel columnLabel = new JLabel("Column count:");
@@ -24,4 +34,7 @@ public class InitialFrame extends JFrame {
 
     }
 
+    public JPanel getPnlMain() {
+        return pnlMain;
+    }
 }
