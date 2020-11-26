a881edcc5ab4abe2c2bfbec40d85e53573e623e0
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-26 09:51:38 -0500

    just a start

--- project3/src/MenuPanel.java
+++ project3/src/MenuPanel.java
@@ -1,2 +1,15 @@
-public class  MenuPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class  MenuPanel{
+
+    private final JPanel pnlMain;
+
+    public MenuPanel(MainFrame frame) {
+
+        pnlMain = new JPanel();
+        pnlMain.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+    }
 }
