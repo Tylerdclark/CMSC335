75039fe9063fba9eae70d10a442c41727ed35f27
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-26 10:18:25 -0500

    This looks okay

--- project3/src/MenuPanel.java
+++ project3/src/MenuPanel.java
@@ -5,11 +5,41 @@ public class  MenuPanel{
 
     private final JPanel pnlMain;
 
-    public MenuPanel(MainFrame frame) {
+    public MenuPanel() {
 
         pnlMain = new JPanel();
         pnlMain.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
+        JButton startBtn = new JButton("Start");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        pnlMain.add(startBtn, gbc);
+
+        JButton pauseBtn = new JButton("Pause");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        pnlMain.add(pauseBtn, gbc);
+
+        JButton stopBtn = new JButton("Stop");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        pnlMain.add(stopBtn, gbc);
+
+        JButton addBtn = new JButton("Add a car");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        pnlMain.add(addBtn, gbc);
+
+    }
+    public JPanel getPnlMain() {
+        return pnlMain;
     }
 }
