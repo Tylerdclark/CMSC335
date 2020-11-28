a202b66e5c5db6ce76e498fa17a9eaa43fd06b7f
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-11-27 21:45:07 -0500

    for now

--- project3/src/Background.java
+++ project3/src/Background.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class background {
+import javax.swing.*;
+import java.awt.*;
+
+public class Background {
+     JPanel pnlMain;
+
+     Background() {
+         pnlMain = new JPanel();
+         pnlMain.setBackground(Color.BLACK);
+         /* In the future, will set size based on how many streets the program will need. */
+         pnlMain.setPreferredSize(new Dimension(350,350));
+     }
+
+    public JPanel getPnlMain() {
+        return pnlMain;
+    }
 }
