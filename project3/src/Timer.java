dc9c8237ed29156158262508d781ee18b2da56ea
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-12-01 21:31:20 -0500

    going to make it extend SwingWorker because it needs to be a thread

--- project3/src/Timer.java
+++ project3/src/Timer.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Timer {
+import javax.swing.*;
+
+public class Timer extends SwingWorker<Void, Void> {
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * <p>
+     * Note that this method is executed only once.
+     *
+     * <p>
+     * Note: this method is executed in a background thread.
+     *
+     * @return the computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    protected Void doInBackground() throws Exception {
+        return null;
+    }
 }
