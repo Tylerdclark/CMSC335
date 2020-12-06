0c766b6e6f5165eeb70dc48e322606464383fe21
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-12-05 21:19:51 -0500

    just mocking up the roads

--- project3/src/Background.java
+++ project3/src/Background.java
@@ -1,17 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Background {
-     JPanel pnlMain;
+public class Background extends JPanel{
 
      Background() {
-         pnlMain = new JPanel();
-         pnlMain.setBackground(Color.BLACK);
+         this.setBackground(Color.BLACK);
          /* In the future, will set size based on how many streets the program will need. */
-         pnlMain.setPreferredSize(new Dimension(350,350));
+         this.setPreferredSize(new Dimension(350,350));
+         this.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+
      }
 
-    public JPanel getPnlMain() {
-        return pnlMain;
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.white);
+        g.drawRect(this.getWidth()/2,0, 10,this.getHeight());
+        g.drawRect(0,this.getHeight()/2,this.getWidth(), 10);
+
     }
 }
