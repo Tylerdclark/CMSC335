0c2de76325915d91bc6fe55488e9ab440c57513a
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-12-09 23:58:36 -0500

    figured out how to paint the roads, but now I need to resize the frame

--- project3/src/gui/BackgroundCanvas.java
+++ project3/src/gui/BackgroundCanvas.java
@@ -1,63 +1,79 @@
 package gui;
 
+import traffic.Car;
+import traffic.EastWestRoad;
 import traffic.NorthSouthRoad;
+import traffic.Road;
+import util.Timer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class BackgroundCanvas extends JPanel{
 
-    private int rowCount, columnCount, carCount;
+    private final int rowCount;
+    private final int columnCount;
+    private final int carCount;
+    private final int width, height;
+    ArrayList<Road> roads = new ArrayList<>();
+    Timer timer;
 
-     BackgroundCanvas() {
+     BackgroundCanvas(int rowCount, int columnCount, int carCount) {
+         this.rowCount = rowCount;
+         this.columnCount = columnCount;
+         this.carCount = carCount;
+         this.width = columnCount * 100;
+         this.height = rowCount * 100;
+
          this.setBackground(Color.BLACK);
-         /* In the future, will set size based on how many streets the program will need. */
-         this.setPreferredSize(new Dimension(350,350));
          this.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+         this.setSize(new Dimension(width, height));
+         this.setPreferredSize(new Dimension(width, height));
+         initialize();
      }
-
-     /* Will paint the cars in a method */
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        paintRoads(g);
+        roads.forEach(road -> road.draw(g));
 
+        System.out.println(this.getHeight());
+        System.out.println(this.getWidth());
     }
 
-    private void paintRoads(Graphics g) {
-        int canvasWidth = this.getWidth();
-        int canvasHeight = this.getHeight();
+    public void populateCars(){
+        Random random = new Random();
+        for (int i = 0; i < carCount ; i++) {
+            Road road = roads.get(random.nextInt(roads.size()));
+            road.addCar(new Car(road, timer));
+        }
+    }
 
-        int firstX = canvasWidth / (columnCount+1);
-        int firstY = canvasHeight / (rowCount+1);
+    public void passTimer(Timer timer){
+        this.timer = timer;
+    }
 
+
+    private void initialize() {
+
+        int firstX = width / (columnCount+1);
+        int firstY = height / (rowCount+1);
 
         for (int i = 1; i <= this.columnCount; i++) {
             int currentRoadX = firstX * i;
-
-            NorthSouthRoad road = new NorthSouthRoad(currentRoadX, canvasHeight);
-            road.draw(g);
+            NorthSouthRoad road = new NorthSouthRoad(currentRoadX, height);
+            roads.add(road);
 
         }
 
         for (int i = 1; i <= this.rowCount; i++) {
             int currentRoadY = firstY * i;
+            EastWestRoad road = new EastWestRoad(currentRoadY, width);
+            roads.add(road);
 
-            g.setColor(Color.white);
-            g.drawLine(0, currentRoadY , canvasWidth, currentRoadY );
         }
-
-    }
-
-    public void setRowCount(int rowCount){
-         this.rowCount = rowCount;
-    }
-    public void setColumnCount(int columnCount){
-         this.columnCount = columnCount;
-    }
-
-    public void setCarCount(int carCount) {
-        this.carCount = carCount;
+        populateCars();
     }
 }
