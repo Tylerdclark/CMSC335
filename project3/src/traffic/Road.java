9caa04dcc316a9b496888c3ba33e1f8b7a3bc647
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-12-10 22:09:40 -0500

    Made this abstract for reasons

--- project3/src/traffic/Road.java
+++ project3/src/traffic/Road.java
@@ -1,5 +1,19 @@
 package traffic;
 
-public class Road {
+import java.awt.*;
+import java.util.ArrayList;
+
+public abstract class Road {
+
+    ArrayList<Car> cars = new ArrayList<>();
     int length;
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+    public ArrayList<Car> getCars(){
+        return this.cars;
+    }
+
+    public abstract void draw(Graphics g);
 }
