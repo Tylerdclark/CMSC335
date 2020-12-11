8a8bdbd683fe988168fdbda30d030ceb8ab23a4c
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-12-10 22:11:47 -0500

    If there is a god and he codes, he will send me to hell for this mess

--- project3/src/traffic/Car.java
+++ project3/src/traffic/Car.java
@@ -4,17 +4,43 @@ import util.Timer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class Car extends SwingWorker<Void, Void> {
 
-    private final Road road;
-    private final util.Timer timer;
+    private static int counter = 0;
 
-    Color color = new Color((int)(Math.random() * 0x1000000));
+    private final Road road;
+    private util.Timer timer;
+    private final Color color = new Color((int)(Math.random() * 0x1000000));
+    private int x, y;
+    Random random;
 
     public Car(Road road, Timer timer){
-        this.road = road;
         this.timer = timer;
+        this.road = road;
+        counter++;
+        random = new Random();
+
+        if (road instanceof NorthSouthRoad){
+            if (random.nextBoolean()){
+                this.x = ((NorthSouthRoad) road).rightSidePathX;
+                this.y = road.length-10;
+            }else{
+                this.x = ((NorthSouthRoad) road).leftSidePathX;
+                this.y = 0;
+            }
+        }
+        if (road instanceof EastWestRoad){
+
+            if (random.nextBoolean()){
+                this.x = 0;
+                this.y = ((EastWestRoad) road).rightSidePathY;
+            } else {
+                this.x = road.length-10;
+                this.y = ((EastWestRoad) road).leftSidePathY;
+            }
+        }
     }
 
     /**
@@ -31,13 +57,32 @@ public class Car extends SwingWorker<Void, Void> {
      */
     @Override
     protected Void doInBackground() throws Exception {
+
+        // going to have to do stuff with the road here
         while (!timer.isStop()) {
             if (!timer.isPause()) {
                 /*Do car stuff here*/
-                System.out.println("i'm riding downtown in " + color + " color.");
+                if (this.road instanceof NorthSouthRoad){
+                    y+=10;
+                }
+                if (this.road instanceof EastWestRoad){
+                    x+=10;
+                }
+                Thread.sleep(1000);
             }
 
         }
         return null;
     }
+
+    public void draw(Graphics graphics){
+        graphics.setColor(this.color);
+        graphics.fillRect(this.x, this.y,10,10 );
+    }
+
+    public void passTimer(Timer timer){
+        this.timer = timer;
+    }
+
+
 }
