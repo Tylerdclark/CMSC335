90e965beb33ea2977c8f501a227bf32dda06714e
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-12-10 22:09:07 -0500

    This is a start. I should work on this tomorrow

--- project3/src/traffic/TrafficLight.java
+++ project3/src/traffic/TrafficLight.java
@@ -1,2 +1,27 @@
-package traffic;public class TrafficLight {
+package traffic;
+
+import javax.swing.*;
+import java.util.Timer;
+
+public class TrafficLight extends SwingWorker<Void, Void> {
+
+   // public TrafficLight(Timer timer, )
+
+
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * <p>
+     * Note that this method is executed only once.
+     *
+     * <p>
+     * Note: this method is executed in a background thread.
+     *
+     * @return the computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    protected Void doInBackground() throws Exception {
+        return null;
+    }
 }
