30c1b805dd170c6b41039a8069c3f1a306652651
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-12-13 23:45:53 -0500

    Super useful class

--- project3/src/util/LightStatus.java
+++ project3/src/util/LightStatus.java
@@ -1,2 +1,17 @@
-package util;public enum LightStatus {
+/*
+ * *****************************************************************************
+ * FILE: LightStatus.java
+ * NAME: Tyler D Clark
+ * PROJECT: Project 3
+ * COURSE: CMSC 335
+ * DATE: 13 Dec 2020
+ * *****************************************************************************
+ */
+package util;
+
+/**
+ * Indispensable enum class to be used by TrafficLight class
+ */
+public enum LightStatus {
+    GO, STOP //wow so useful
 }
